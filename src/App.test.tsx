fb45e8779539cf4764f2392b0f5d9ecfd9755497
whabbot <whabbot0@gmail.com>
2024-03-06 01:01:57 +0000

    Initial commit

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,40 @@
+
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('renders form fields', () => {
+  render(<App />);
+  const requiredLetterInput = screen.getByLabelText(/required letter/i);
+  const allLettersInput = screen.getByLabelText(/all letters/i);
+  const submitButton = screen.getByRole('button', { name: /submit/i });
+  const resetButton = screen.getByRole('button', { name: /reset/i });
+
+  expect(requiredLetterInput).toBeInTheDocument();
+  expect(allLettersInput).toBeInTheDocument();
+  expect(submitButton).toBeInTheDocument();
+  expect(resetButton).toBeInTheDocument();
+});
+
+test('submits form and displays words', () => {
+  render(<App />);
+  const requiredLetterInput = screen.getByLabelText(/required letter/i);
+  const allLettersInput = screen.getByLabelText(/all letters/i);
+  const submitButton = screen.getByRole('button', { name: /submit/i });
+
+  fireEvent.change(requiredLetterInput, { target: { value: 'a' } });
+  fireEvent.change(allLettersInput, { target: { value: 'abcde' } });
+  fireEvent.click(submitButton);
+
+  const pangrams = screen.getByText(/pangrams/i);
+  const otherWords = screen.getByText(/other words/i);
+
+  expect(pangrams).toBeInTheDocument();
+  expect(otherWords).toBeInTheDocument();
 });
---
+++ src/wordSuggestions.ts
@@ -0,0 +1,29 @@
+import dictionary from './assets/dictionary.json';
+
+type Dictionary = {
+    words: string[];
+}
+
+type Words = {
+    pangrams: string[];
+    otherWords: string[];
+};
+
+const dictionaryData = dictionary as Dictionary;
+
+export function getWords(requiredLetter: string, letterList: string[]): Words {
+    const words: Words = { pangrams: [], otherWords: [] };
+    dictionaryData.words.forEach(word => {
+        const uniqueWordLetters = new Set(word.split(''));
+        const matchingLetters = letterList.filter(letter => uniqueWordLetters.has(letter));
+        if (uniqueWordLetters.has(requiredLetter) && matchingLetters.length === uniqueWordLetters.size) {
+            words.otherWords.push(word)
+            if (letterList.length === uniqueWordLetters.size) {
+                words.pangrams.push(word)
+            };
+        };
+
+    });
+    return words;
+}
+
